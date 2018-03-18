65b9f9fd61e69d32a951d37c341dfd6df0c71157
agam98 <dwivedi.agam233@gmail.com>
2018-03-18 08:53:21 +0530

    New Image Processing ALgo

--- weights.py
+++
@@ -1,9 +0,0 @@
-from keras.models import model_from_json
-from keras.models import load_model
-
-model_json = load_model.to_json()
-with open("model.json", "w") as json_file:
-    json.file_write(model_json)
-
-model.save_weights("model.h5")
-print("saved model to disk")
